9365e3e3fa9e82735fb26646392a8b3545000aa8
unknown <akash.tiwari21aug@gmail.com>
2021-04-15 13:01:33 +0530

    added comments and cleaned structure of load balancer

--- LB/load.py
+++ LB/load.py
@@ -6,21 +6,7 @@ from threading import Timer, Thread, Lock
 from Messages.edge_LB import *
 from Messages.client_LB import *
 import time
-
-
-LOAD_IP = '127.0.0.1'
-EDGE_PORT = 6542
-CLIENT_PORT = 6543
-EDGE_MAX = 100
-WEIGHT_DISTANCE = 0.8
-WEIGHT_LOAD = 0.2
-MAX_CLIENT_REQUESTS= 10
-
-LOCATION = {}
-LOCATION[0] = (0.,0.)
-LOCATION[1] = (0.,1.)
-LOCATION[2] = (1.,0.)
-LOCATION[3] = (1.,1.)
+from constants import *
 
 # locks and dicts
 edge_avail_dict = []
@@ -28,18 +14,19 @@ edge_avail_lock  = Lock()
 edge_load_dict = {}
 edge_load_lock = Lock()
 
-#
+# Handles incoming connections from edge server
 def establish_edge():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = LOAD_IP
-    port = EDGE_PORT
+    # host = LOAD_IP
+    # port = EDGE_PORT
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((host, port))
+    sock.bind((LOAD_IP, EDGE_PORT))
 
     sock.listen(EDGE_MAX)
 
     threads = []
 
+    # for every incoming connection create a thread
     while(True):
         conn, addr = sock.accept()
         t= Thread(target = recv_edge , args = (conn,addr))
@@ -47,76 +34,93 @@ def establish_edge():
         t.start()
     sock.close()
 
+    # close the threads
     for t in threads:
         t.join()
 
+# edge server message handler
 def recv_edge(conn,addr):
     # define edge to Load balancer message
     msg = Edge_LB()
     msg.receive(conn)
     prev_load = -1
 
-    if msg.received:
+    # initial message handler
+    if (msg.received):
         print("New edge server connected", addr)
-        # prev_load = msg.load
+        # atomically add the load of edge server to dict
         edge_load_lock.acquire()
         edge_load_dict[addr] = msg.load
         edge_load_lock.release()
+        
+        # prev load keeps track of last known load of the edge server
         prev_load = msg.load
+
+        # atomically add edge server to liust of availible edge severs
         edge_avail_lock.acquire()
         edge_avail_dict.append((msg.loc, addr,msg.load))
         edge_avail_lock.release()
     
+    # for every consequtive message received
     while True:
         msg = Edge_LB()
         msg.receive(conn)
         if not msg.received:
             break
+        # if load has changed
         elif prev_load!=msg.load:
+            # atomically update load
             edge_load_lock.acquire()
             edge_load_dict[addr] = msg.load
             edge_load_lock.release()
             prev_load = msg.load
          
-        print("Heartbeat received from", addr)
+        print("Message received from edge server - ", addr)
 
+    # if message is not received
+    # search for the edge server
     for e,a in enumerate(edge_avail_dict):
         if a[1] == addr:
+            # atomically pop it from the dict
             edge_avail_lock.acquire()
             edge_avail_dict.pop(e)
             edge_avail_lock.release()
+            # remove corresponding load
             edge_load_lock.acquire()
             del edge_load_dict[addr]
             edge_load_lock.release()
-            print("connection closed")
+            print("connection closed from edge server - ", addr)
             break
+    # close the connection
     conn.close()
 
+# gets the dist between two local Ids
 def dist(loc_id1, loc_id2):
-	global LOCATION
-	print(LOCATION[loc_id1])
 	return (LOCATION[loc_id1][0]-LOCATION[loc_id2][0])**2 + (LOCATION[loc_id1][1]-LOCATION[loc_id2][1])**2
 
+# handles request from clients
 def rcv_client(conn,addr):
     msg  = Client_LB_req()
     msg.receive(conn)
 
     if(msg.received):
-        print("Received request: loc id ", msg.loc_id, " from ", addr)
+        print("Received request from client: loc id ", msg.loc_id, " from ", addr)
 
         loc_id = msg.loc_id
         # look in edge_servers_available after acquiring lock
         edge_avail_lock.acquire()
-        # At least one edge server would be available
-        if(len(edge_avail_dict)==1):
-            msg = Client_LB_res(*edge_avail_dict[0][1])
+        # search for availaible edge servers
+
+        # no edge servers availaible
+        if(len(edge_avail_dict)==0):
+            msg = Client_LB_res('0.0.0.0',EDGE_PORT)
             edge_avail_lock.release()
             msg.send(conn)
             conn.close()
             return
-
-        if(len(edge_avail_dict)==0):
-            msg = Client_LB_res('0.0.0.0',EDGE_PORT)
+        # only 1 edge server avalaible
+        elif(len(edge_avail_dict)==1):
+            msg = Client_LB_res(*edge_avail_dict[0][1])
             edge_avail_lock.release()
             msg.send(conn)
             conn.close()
@@ -126,28 +130,27 @@ def rcv_client(conn,addr):
         cur_load = sys.maxsize
         best_server_index = 0
         
+        # find the best edge sever
         for e,server in enumerate(edge_avail_dict):
-            
+            # if prev edge server is availible, assign it
             if server[1]==msg.prev_edge_ip:
-
+                # ???
                 if e == 0:
                     best_server_index = 1
                 continue
-            
+            # calculate dist from selected edge server
             cur_dist = dist(server[0], loc_id)
-            edge_load_lock.acquire()
             
+            edge_load_lock.acquire()
+            # check if weightage of load and dist is less than prev
             if WEIGHT_DISTANCE*min_dist+WEIGHT_LOAD*cur_load > WEIGHT_DISTANCE*cur_dist+WEIGHT_LOAD*edge_load_dict[edge_avail_dict[e][1]]:
                 min_dist = cur_dist
                 cur_load = edge_load_dict[edge_avail_dict[e][1]]
                 best_server_index = e
-            
             edge_load_lock.release()
         
-        
-
+        # send best availaible edge server
         msg = Client_LB_res(edge_avail_dict[best_server_index][1][0],edge_avail_dict[best_server_index][1][1])
-        print(edge_avail_dict[best_server_index][1][1])
         edge_avail_lock.release()
         
         msg.send(conn)
