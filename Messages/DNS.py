270067c4b80bde9d78fc3bc330749d62a69ce1ac
unknown <akash.tiwari21aug@gmail.com>
2021-04-16 01:34:52 +0530

    dns respnse method added

--- Messages/DNS.py
+++ Messages/DNS.py
@@ -4,6 +4,14 @@ sys.path.insert(0, "../")
 from constants import *
 from struct import *
 
+def stream_ip(ip):
+	ip = ip.split('.')
+	ip = [int(i).to_bytes(1, 'big') for i in ip]
+	return ip
+
+def unstream_ip(ip0, ip1, ip2, ip3):
+	return str(int.from_bytes(ip0, 'big')) + "." + str(int.from_bytes(ip1, 'big')) + "." + str(int.from_bytes(ip2, 'big')) + "." + str(int.from_bytes(ip3, 'big'))
+
 class DNSreq():
 	signature = 'B'+str(MAXLEN_DOMAIN)+'s4cH'
 	size = calcsize(signature)
@@ -37,3 +45,25 @@ class DNSreq():
 			self.hostname = hostname.decode().strip()
 			self.ip = str(int.from_bytes(ip0, 'big')) + "." + str(int.from_bytes(ip1, 'big')) + "." + str(int.from_bytes(ip2, 'big')) + "." + str(int.from_bytes(ip3, 'big'))
 			self.port = port
+
+class DNSres():
+
+	"""
+	ipblocks - <ip,port> x 2
+	"""
+
+	signature = '4cH4cH'
+	size = calcsize(signature)
+
+	def __init__(self, ipblocks = None):
+		self.ipblocks = ipblocks
+
+	def send(self, soc):
+		ip0 = stream_ip(self.ipblocks[0][0])
+		ip1 = stream_ip(self.ipblocks[1][0])
+		soc.send(pack(DNSres.signature, ip0[0], ip0[1], ip0[2], ip0[3], self.ipblocks[0][1], ip1[0], ip1[1], ip1[2], ip1[3], self.ipblocks[1][1]))
+
+	def receive(self, soc):
+		arr = soc.recv(DNSres.size)
+		ip00, ip01, ip02, ip03, port0, ip10, ip11, ip12, ip13, port1 = unpack(DNSres.signature, arr)
+		self.ipblocks = [(unstream_ip(ip00, ip01, ip02, ip03), port0), (unstream_ip(ip10, ip11, ip12, ip13), port1)]
